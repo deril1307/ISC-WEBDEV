118636e327db54347d29cd22c8a074283bcad774
deril1307 <derilwijdan346@gmail.com>
2024-07-29 21:12:25 +0700

    menambahkan weather

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import fetch from "node-fetch";
+import cors from "cors";
+
+const app = express();
+const port = 3000;
+const apiKey = "a42c2709196635074c089d59efebd744";
+const cityId = "1642911"; // ID kota Jakarta
+
+app.use(cors());
+
+app.get("/weather", async (req, res) => {
+  try {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?id=${cityId}&appid=${apiKey}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    res.status(500).send("Error fetching weather data");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
