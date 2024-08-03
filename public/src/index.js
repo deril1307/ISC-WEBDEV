6c59eeeb4f748edc081dd7a5e5fa1a7e4e2a6cc2
deril1307 <derilwijdan346@gmail.com>
2024-08-03 23:27:40 +0700

    menambahkan our service more dan about us

--- public/src/index.js
+++ public/src/index.js
@@ -62,3 +62,21 @@ document.addEventListener("DOMContentLoaded", function () {
     showNotification(`Query parameter 'example' is ${someParam}`);
   }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll(".fade-in");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      } else {
+        entry.target.classList.remove("visible");
+      }
+    });
+  });
+
+  elements.forEach((element) => {
+    observer.observe(element);
+  });
+});
